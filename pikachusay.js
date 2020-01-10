f03d7f0c927e4e68345eb629f4dfa5b70be79b5a
Monica Rao <monicaxrao@gmail.com>
2020-01-10 11:20:08 -0500

    first commit

---
+++ pikachusay.js
@@ -0,0 +1,14 @@
+const list = process.argv.slice(2);
+let pikaSay = [];
+for (let word of list) {
+    const repeat = Math.ceil(word.length / 4);
+    const pikaWord = 'pika'.repeat(repeat);
+    pikaSay.push(pikaWord);
+}
+console.log(`
+
+(( ${pikaSay.join(' ')} ))
+      \\
+        \/\\---\/\\
+     ϞϞ(๑⚈ ․̫ ⚈๑)∩
+`);
