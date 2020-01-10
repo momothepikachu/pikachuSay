3b5db05ec4251261d07ca5a2311ea447a11db148
Monica Rao <monicaxrao@gmail.com>
2020-01-10 11:32:53 -0500

    feat: add bin

--- pikachusay.js
+++ pikachusay.js
@@ -1,9 +1,11 @@
+#!/usr/bin/env node
+
 const list = process.argv.slice(2);
 let pikaSay = [];
 for (let word of list) {
-    const repeat = Math.ceil(word.length / 4);
-    const pikaWord = 'pika'.repeat(repeat);
-    pikaSay.push(pikaWord);
+  const repeat = Math.ceil(word.length / 4);
+  const pikaWord = 'pika'.repeat(repeat);
+  pikaSay.push(pikaWord);
 }
 console.log(`
 
